a977b580585faeda25ba3491f31b6576927c2b47
Nizam <ramazanov570633@gmail.com>
2023-10-27 20:55:29 +0400

    Onboarding

--- src/Page/Onboarding/Onboarding.tsx
+++ src/Page/Onboarding/Onboarding.tsx
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity } from 'react-native'
 import React from 'react'
-
-const Onboarding = () => {
-  return (
-    <View>
-      <Text>Onboarding</Text>
-    </View>
-  )
+import onboradingcss from './onboradingcss'
+const Onboarding = ({navigation}:any) => {
+    return (
+        <View style={onboradingcss.view}>
+            <View>
+                <Text style={onboradingcss.text}>Welcome</Text>
+                <Text style={onboradingcss.text1}>Please click the Start button to begin</Text>
+            </View>
+            <View style={onboradingcss.buttonview}>
+                <TouchableOpacity style={onboradingcss.touc} onPress={() => navigation.navigate('HomeScreen', { screen: 'Home' })}>
+                    <Text style={onboradingcss.text2}>Start</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
 }
 
 export default Onboarding
---
+++ src/Page/Onboarding/onboradingcss.tsx
@@ -0,0 +1,37 @@
+import { StyleSheet } from 'react-native'
+export default StyleSheet.create({
+    view: {
+        backgroundColor: "#333B4E",
+        flex: 1
+    },
+    text: {
+        fontSize: 30,
+        width: '90%',
+        textAlign: 'center',
+        marginLeft: '5%',
+        marginTop:'50%'
+
+    },
+    text1: {
+        fontSize: 20,
+        width: '90%',
+        textAlign: 'center',
+        marginTop: '5%',
+        marginLeft: '5%'
+    },
+    buttonview:{
+        marginTop:'35%',
+        alignItems:'center'
+    },
+    touc: {
+        backgroundColor: '#435264',
+        width: 200,
+        height: 70,
+        borderRadius: 10
+    },
+    text2: {
+        fontSize: 30,
+        textAlign: 'center',
+        marginTop: '2%'
+    }
+})
